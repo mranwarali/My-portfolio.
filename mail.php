60d991e0643ebe5af7efd02071b98e28accfc15b
Anwar Ali <101239418+AnwarAli-786@users.noreply.github.com>
2022-03-10 10:13:56 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "admin@devitems.com";
+        $recipient = "ali194377@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
